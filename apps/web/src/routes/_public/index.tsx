f8e5ac45bc10abd6c2e189bdc32ed09413a5e5b8
Andras Serfozo <subztep@gmail.com>
2026-04-17 07:15:37 +0100

    bg anim

--- apps/web/src/routes/_public/index.tsx
+++ apps/web/src/routes/_public/index.tsx
@@ -6,20 +6,20 @@ export const Route = createFileRoute("/_public/")({ component: App })
 
 function App() {
   return (
-    <Section className="w-96 flex flex-col gap-4 transform-[perspective(900px)_rotateY(10deg)_rotateX(4deg)] hover:shadow-purple-800/69">
+    <Section className="w-96 flex flex-col gap-4 opacity-80 transform-[perspective(900px)_rotateY(10deg)_rotateX(4deg)] hover:shadow-purple-800/69">
       <h1>Hello, World!</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
         magna aliqua.
       </p>
       <Link to="/signin">
-        <Button variant="primary" className="w-full">
+        <Button variant="primary" size="lg" className="w-full">
           Sign In
         </Button>
       </Link>
       <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       <Link to="/signup">
-        <Button variant="primary" className="w-full">
+        <Button variant="primary" size="lg" className="w-full">
           Sign Up
         </Button>
       </Link>
