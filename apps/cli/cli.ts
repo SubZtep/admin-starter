04e148127c5b03a0b9225d519aff1fe2b2e23216
Andras Serfozo <subztep@gmail.com>
2026-04-24 11:12:15 +0100

    await's async

--- apps/cli/cli.ts
+++ apps/cli/cli.ts
@@ -12,42 +12,44 @@ Commands:
   process.exit()
 }
 
-intro(`${lime}Welcome to Kaja CLI 🚀`)
+void (async () => {
+  intro(`${lime}Welcome to Kaja CLI 🚀`)
 
-await auth.authFlow()
+  await auth.authFlow()
 
-// await cli.validateConnections()
-// await kaja.registerNode({ name: "no-name" })
-// if (!kaja.nodeId) {
-//   console.error("Node ID is required to submit results")
-//   process.exit(1)
-// }
+  // await cli.validateConnections()
+  // await kaja.registerNode({ name: "no-name" })
+  // if (!kaja.nodeId) {
+  //   console.error("Node ID is required to submit results")
+  //   process.exit(1)
+  // }
 
-// await cli.beatingHeart()
-// let job: JobData | undefined
+  // await cli.beatingHeart()
+  // let job: JobData | undefined
 
-// do {
-//   job = await cli.waitingForJobs()
-//   if (job) {
-//     // @ts-expect-error status is required
-//     const result: SubmitResultRequest = {
-//       nodeId: kaja.nodeId,
-//       jobId: job.id
-//     }
+  // do {
+  //   job = await cli.waitingForJobs()
+  //   if (job) {
+  //     // @ts-expect-error status is required
+  //     const result: SubmitResultRequest = {
+  //       nodeId: kaja.nodeId,
+  //       jobId: job.id
+  //     }
 
-//     try {
-//       const text = await cli.workingOnJob(job)
-//       result.status = "success"
-//       result.result = { text }
-//     } catch (error: any) {
-//       console.error(`Error working on job: ${error.message}`)
-//       result.status = "error"
-//       result.error = error.message
-//     }
+  //     try {
+  //       const text = await cli.workingOnJob(job)
+  //       result.status = "success"
+  //       result.result = { text }
+  //     } catch (error: any) {
+  //       console.error(`Error working on job: ${error.message}`)
+  //       result.status = "error"
+  //       result.error = error.message
+  //     }
 
-//     await kaja.submitResult(result)
-//   }
-// } while (job)
+  //     await kaja.submitResult(result)
+  //   }
+  // } while (job)
 
-outro(`${purple}Farewell 🫠`)
-process.exit()
+  outro(`${purple}Farewell 🫠`)
+  process.exit()
+})()
